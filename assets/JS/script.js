f3794918d8ea5105b5d610ae993e2cabc2912662
Kaleb Muse <kcmuse@Kalebs-MacBook-Air.local>
2021-07-05 19:17:17 -0700

    adjusted code to match criteria

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,100 +1,111 @@
 var userFormEl = document.querySelector('#user-form');
 var getWeatherBtn = document.getElementById(`getweather`);
-var currentEl = document.getElementById(`active`)
 var apiKey = "4f85dc9c3f81dd9dc3c9df75d407bc88";
-var currentTime = moment().format(`MMMM Do, YYYY LT`)
-var dayOfWeek = moment().format(`dddd`);
-var weatherWindow = document.getElementById(`weather`);
-
-
 var userInput = document.getElementById(`cityname`);
 
 
 function getWeatherToday() {
-    if (weatherWindow.style.display !== `block`) {
-        weatherWindow.style.display = `block`;
-    }
     var cityName = userInput.value;
     var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=` + cityName + `&appid=` + apiKey + `&units=imperial`;
     fetch(apiUrl)
         .then(function (response) {
-            console.log(response)
             return response.json();
         })
         .then(function (data) {
             console.log(data);
-            // Grabbing the sections that need to be changed
-            var today = document.querySelector(`.today`);
-            var tempToday = document.querySelector(`.temptoday`);
-            var windToday = document.querySelector(`.windtoday`);
-            var humidToday = document.querySelector(`.humidtoday`);
-            // Setting the context of each section to the values within the api
-            today.textContent = data.name + ` ` + currentTime;
-            tempToday.textContent = `Temperature: ` + data.main.temp + `\u00B0 F`;
-            windToday.textContent = `Wind Speed: ` + data.wind.speed + ` MPH`;
-            humidToday.textContent = `Humidity: ` + data.main.humidity + `%`;
-        }
+            var humidity = data.main.humidity;
+            var wind = data.wind.speed;
+            var temp = data.main.temp_max;
+            var currentTime = moment().format(`MMMM Do, YYYY LT`)
+            var icon = data.weather[0].icon;
+            var iconImg = `http://openweathermap.org/img/wn/${icon}@2x.png`
+            var current = document.querySelector(`#current`)
+            var card = document.createElement(`div`);
+            var cardHeader = document.createElement(`h2`);
+            var cardBody = document.createElement(`div`);
+            var humid = document.createElement(`p`);
+            var temper = document.createElement(`p`);
+            var windy = document.createElement(`p`);
+            var weatherIcon = document.createElement(`img`);
+
+            card.classList.add(`card`, `m-3`, `w-100`);
+            cardHeader.classList.add(`card-header`);
+            cardBody.classList.add(`card-body`);
+            humid.classList.add(`card-text`);
+            temper.classList.add(`card-text`);
+            windy.classList.add(`card-text`);
+
+            cardHeader.textContent = data.name + ` ` + currentTime;
+            humid.textContent = `Humidity: ` + humidity + `%`;
+            temper.textContent = `High Temperature: ` + temp + `\u00B0 F`;
+            windy.textContent = `Wind Speed: ` + wind + `MPH`;
+            weatherIcon.src = iconImg;
+
+            current.appendChild(card);
+            card.appendChild(cardHeader);
+            card.appendChild(cardBody);
+            cardBody.appendChild(weatherIcon);
+            cardBody.appendChild(temper);
+            cardBody.appendChild(windy);
+            cardBody.appendChild(humid);
+
+            getFiveDay(data);
+
         })
+
 }
-function getFiveDay() {
-    var cityName = userInput.value;
-    var apiUrlFiveDay = `https://api.openweathermap.org/data/2.5/forecast?q=` + cityName + `&appid=` + apiKey + `&units=imperial`;
+
+function getFiveDay(res) {
+    var lat = res.coord.lat;
+    var lon = res.coord.lon;
+    var city = res.name;
+    var apiUrlFiveDay = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${apiKey}&units=imperial`
     fetch(apiUrlFiveDay)
         .then(function (response) {
-            console.log(response)
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
-            var dayOne = document.querySelector(`.day1`);
-            var tempOne = document.querySelector(`.temp1`);
-            var windOne = document.querySelector(`.wind1`);
-            var humidOne = document.querySelector(`.humid1`);
-            // Setting the context of each section to the values within the api
-            dayOne.textContent = dayOfWeek;
-            tempOne.textContent = `Temp: ` + data.list[1].main.temp + `\u00B0 F`;
-            windOne.textContent = `Wind: ` + data.list[1].wind.speed + ` MPH`;
-            humidOne.textContent = `Humid: ` + data.list[1].main.humidity + `%`;
-            //day 2
-            var dayTwo = document.querySelector(`.day2`);
-            var tempTwo = document.querySelector(`.temp2`);
-            var windTwo = document.querySelector(`.wind2`);
-            var humidTwo = document.querySelector(`.humid2`);
-            // Setting the context of each section to the values within the api
-            dayTwo.textContent = dayOfWeek;
-            tempTwo.textContent = `Temp: ` + data.list[9].main.temp + `\u00B0 F`;
-            windTwo.textContent = `Wind: ` + data.list[9].wind.speed + ` MPH`;
-            humidTwo.textContent = `Humid: ` + data.list[9].main.humidity + `%`;
-            // day3
-            var dayThree = document.querySelector(`.day3`);
-            var tempThree = document.querySelector(`.temp3`);
-            var windThree = document.querySelector(`.wind3`);
-            var humidThree = document.querySelector(`.humid3`);
-            // Setting the context of each section to the values within the api
-            dayThree.textContent = dayOfWeek;
-            tempThree.textContent = `Temp: ` + data.list[17].main.temp + `\u00B0 F`;
-            windThree.textContent = `Wind: ` + data.list[17].wind.speed + ` MPH`;
-            humidThree.textContent = `Humid: ` + data.list[17].main.humidity + `%`;
-            //day 4
-            var dayFour = document.querySelector(`.day4`);
-            var tempFour = document.querySelector(`.temp4`);
-            var windFour = document.querySelector(`.wind4`);
-            var humidFour = document.querySelector(`.humid4`);
-            // Setting the context of each section to the values within the api
-            dayFour.textContent = dayOfWeek;
-            tempFour.textContent = `Temp: ` + data.list[25].main.temp + `\u00B0 F`;
-            windFour.textContent = `Wind: ` + data.list[25].wind.speed + ` MPH`;
-            humidFour.textContent = `Humid: ` + data.list[25].main.humidity + `%`;
-            // day5
-            var dayFive = document.querySelector(`.day5`);
-            var tempFive = document.querySelector(`.temp5`);
-            var windFive = document.querySelector(`.wind5`);
-            var humidFive = document.querySelector(`.humid5`);
-            // Setting the context of each section to the values within the api
-            dayFive.textContent = dayOfWeek;
-            tempFive.textContent = `Temp: ` + data.list[33].main.temp + `\u00B0 F`;
-            windFive.textContent = `Wind: ` + data.list[33].wind.speed + ` MPH`;
-            humidFive.textContent = `Humid: ` + data.list[33].main.humidity + `%`;
+            var daily = data.daily
+            for (var i = 1; i <= daily.length - 3; i++) {
+                var date = moment.unix(daily[i].dt).format(`M/D/YY`)
+                var humidity = daily[i].humidity;
+                var wind = daily[i].wind_speed;
+                var temp = daily[i].temp.max;
+                var icon = daily[i].weather[0].icon;
+
+                var iconImg = `http://openweathermap.org/img/wn/${icon}.png`
+
+                var fiveDay = document.querySelector(`#fiveday`)
+                var card = document.createElement(`div`);
+                var cardHeader = document.createElement(`div`);
+                var cardBody = document.createElement(`div`);
+                var humid = document.createElement(`p`);
+                var temper = document.createElement(`p`);
+                var windy = document.createElement(`p`);
+                var weatherIcon = document.createElement(`img`);
+
+                card.classList.add(`card`, `text-white`, `bg-primary`, `mb-3`, `mx-3`, `cstm-card`);
+                cardHeader.classList.add(`card-header`);
+                cardBody.classList.add(`card-body`);
+                humid.classList.add(`card-text`);
+                temper.classList.add(`card-text`);
+                windy.classList.add(`card-text`);
+                card.style.maxWidth = `18rem`;
+
+                cardHeader.textContent = date;
+                humid.textContent = `Humidity: ` + humidity + `%`;
+                temper.textContent = `High Temp: ` + temp + `\u00B0 F`;
+                windy.textContent = `Wind Speed: ` + wind + `MPH`;
+                weatherIcon.src = iconImg;
+
+                fiveDay.appendChild(card);
+                card.appendChild(cardHeader);
+                card.appendChild(cardBody);
+                cardBody.appendChild(weatherIcon);
+                cardBody.appendChild(temper);
+                cardBody.appendChild(windy);
+                cardBody.appendChild(humid);
+            }
         })
 }
 getWeatherBtn.addEventListener(`click`, getWeatherToday);
